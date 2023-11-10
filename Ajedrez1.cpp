b84233198316d5470e4e4fdb4de448796e810680
natanaelreyes <natab28@gmail.com>
2023-11-10 12:15:32 -0400

    Update Ajedrez1.cpp
    agregando movimiento de piezas

--- Ajedrez1.cpp
+++ Ajedrez1.cpp
@@ -38,11 +38,52 @@ void MostrarTablero(){
     cout << "    [a][b][c][d][e][f][g][h]" << endl;
 }
 
+bool MoverPieza(int filaOrigen, int columnaOrigen, int filaDestino, int columnaDestino) {
+   
+    if (filaOrigen < 0 || filaOrigen > 7 || columnaOrigen < 0 || columnaOrigen > 7 ||
+        filaDestino < 0 || filaDestino > 7 || columnaDestino < 0 || columnaDestino > 7) {
+        return false;
+    }
+
+    
+    if (tablero[filaOrigen][columnaOrigen] == ' ') {
+        return false;
+    }
+
+  
+    tablero[filaDestino][columnaDestino] = tablero[filaOrigen][columnaOrigen];
+    tablero[filaOrigen][columnaOrigen] = ' ';
+
+    return true;
+}
+
+void EntradaTeclado() {
+    int filaOrigen, columnaOrigen, filaDestino, columnaDestino;
+
+    cout << "Ingrese la Fila de Origen: ";
+    cin >> filaOrigen;
+    cout << "Ingrese la Columna de origen: ";
+    cin >> columnaOrigen;
+    cout << " a que fila quieres Moverte: ";
+    cin >> filaDestino;
+    cout << "A que columna Quieres Moverte: ";
+    cin >> columnaDestino;
+
+    if (!MoverPieza(filaOrigen, columnaOrigen, filaDestino, columnaDestino)) {
+        cout << "Movimineto no Permitido!" << endl;
+    }
+}
+
 
 int main() {
     InicializarTablero();
     MostrarTablero();
-	
-	getch(); 
+
+    while (true) {
+        EntradaTeclado();
+        MostrarTablero();
+    }
+
+    getch(); 
     return 0;
 }
